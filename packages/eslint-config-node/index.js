8eaec316c23a25af2185db0c5fc8cbedc403b6e4
ABuffSeagull <reecevanatta@gmail.com>
2019-05-04 17:52:18 -0400

    Add some node rules so I can actually publish

--- packages/eslint-config-node/index.js
+++ packages/eslint-config-node/index.js
@@ -3,4 +3,17 @@ module.exports = {
     node: true
   },
   extends: ['@abuffseagull/eslint-config-base'],
+  plugins: ['node'],
+  rules: {
+    // node.js & commonjs
+    'callback-return': 'error',
+    'global-require': 'error',
+    'handle-callback-err': 'error',
+    'no-buffer-constructor': 'error',
+    'no-mixed-requires': 'error',
+    'no-new-require': 'error',
+    'no-path-concat': 'error',
+    'no-process-exit': 'error',
+    'no-sync': 'warn',
+  }
 }
