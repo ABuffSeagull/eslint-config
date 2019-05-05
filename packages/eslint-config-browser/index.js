2e1dc5d9d3e44124304c7a3b21672b4b2ec44acc
ABuffSeagull <reecevanatta@gmail.com>
2019-05-04 21:12:56 -0400

    Finish up the plugins I started with

--- packages/eslint-config-browser/index.js
+++ packages/eslint-config-browser/index.js
@@ -2,12 +2,16 @@
 module.exports = {
   env: { browser: true },
   extends: ['@abuffseagull/eslint-config-base'],
-  plugins: ['unicorn'],
+  plugins: ['unicorn', 'xss'],
   rules: {
     // unicorn
     'unicorn/prefer-node-append': 'error',
     'unicorn/prefer-node-remove': 'error',
     'unicorn/prefer-query-selector': 'error',
     'unicode/prefer-text-content': 'error',
+
+    // xss
+    'xss/no-mixed-html': 'error',
+    'xss/no-location-href-assign': 'error',
   },
 };
